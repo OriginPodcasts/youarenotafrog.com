81fdd8b55efbaac859c52ac98d9d5fe49a6efd0a
Mark Steadman <mark@origin.fm>
2023-03-23 11:26:08 +0000

    Adds the basic grid structure to the 404 page

--- theme/404.php
+++ theme/404.php
@@ -1,14 +1,22 @@
 <?php get_header(); ?>
 
-<article id="post-0" class="post not-found">
-	<header class="header">
-		<h1 class="entry-title" itemprop="name"><?php esc_html_e('Not Found', 'yanaf'); ?></h1>
-	</header>
-	
-	<div class="entry-content" itemprop="mainContentOfPage">
-		<p><?php esc_html_e('Nothing found for the requested page. Try a search instead?', 'yanaf'); ?></p>
-		<?php get_search_form(); ?>
-	</div>
-</article>
+<div class="grid-container">
+    <div class="grid-x grid-margin-x">
+        <?php get_sidebar(); ?>
+
+        <main class="main cell medium-9">
+            <article id="post-0" class="post not-found">
+				<header class="header">
+					<h1 class="entry-title" itemprop="name"><?php esc_html_e('Not Found', 'yanaf'); ?></h1>
+				</header>
+				
+				<div class="entry-content" itemprop="mainContentOfPage">
+					<p><?php esc_html_e('Nothing found for the requested page. Try a search instead?', 'yanaf'); ?></p>
+					<?php get_search_form(); ?>
+				</div>
+			</article>
+        </main>
+    </div>
+</div>
 
 <?php get_footer();
