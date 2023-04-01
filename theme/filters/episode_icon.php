10f4d00494fc810ae7ab1438c4452e7ffa4d380f
Mark Steadman <mark@origin.fm>
2023-04-01 11:09:54 +0100

    Fixes how link icons are included
    Previously they were being printed via “require” but this is wrong since they should be returned via the function and then echoed by the line calling the function.

--- theme/filters/episode_icon.php
+++ theme/filters/episode_icon.php
@@ -1,8 +1,14 @@
 <?php add_filter('yanaf_episode_icon', 'yanaf_episode_icon');
 function yanaf_episode_icon($icon) {
-    if ($icon === 'calendar') {
-        require(
-            dirname(dirname(__file__)) . '/icons/calendar.svg'
-        );
+    $filename = sprintf(
+        '%s/icons/%s.svg',
+        dirname(dirname(__file__)),
+        $icon
+    );
+
+    if (is_file($filename)) {
+        return file_get_contents($filename);
     }
+
+    return $icon;
 }
