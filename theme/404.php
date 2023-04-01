91519379b318236b1f248fe291b8ba3e1133427a
Mark Steadman <mark@origin.fm>
2023-04-01 13:23:15 +0100

    Restyles the 404 page to be more like a standard page

--- theme/404.php
+++ theme/404.php
@@ -1,22 +1,20 @@
 <?php get_header(); ?>
 
-<div class="grid-container">
-    <div class="grid-x grid-margin-x">
-        <?php get_sidebar(); ?>
+<main>
+    <header class="page-header">
+        <div class="grid-container">
+            <h1 class="page-title"><?php esc_html_e('Page not found', 'yanaf'); ?></h1>
+        </div>
+    </header>
 
-        <main class="main cell medium-9">
-            <article id="post-0" class="post not-found">
-				<header class="header">
-					<h1 class="entry-title" itemprop="name"><?php esc_html_e('Not Found', 'yanaf'); ?></h1>
-				</header>
-				
-				<div class="entry-content" itemprop="mainContentOfPage">
-					<p><?php esc_html_e('Nothing found for the requested page. Try a search instead?', 'yanaf'); ?></p>
-					<?php get_search_form(); ?>
-				</div>
-			</article>
-        </main>
+    <div class="grid-container">
+        <div class="grid-x grid-margin-x">
+            <article class="main single cell medium-9">
+                <p><?php esc_html_e('Nothing found for the requested page. Try a search instead?', 'yanaf'); ?></p>
+				<?php get_search_form(); ?>
+            </article>
+        </div>
     </div>
-</div>
+</main>
 
 <?php get_footer();
