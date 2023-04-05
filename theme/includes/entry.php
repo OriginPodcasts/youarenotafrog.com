f0f62e8ce55330b4c460c9d3376a001b208489bf
Mark Steadman <mark@origin.fm>
2023-04-05 16:30:41 +0100

    Adds a generic entry-in-archive template part

---
+++ theme/includes/entry.php
@@ -0,0 +1,20 @@
+<?php $post_type = get_post_type(); ?>
+<a href="<?php the_permalink(); ?>" class="<?php echo $post_type; ?> entry">
+    <?php yanaf_img_srcset(
+        get_post_thumbnail_id(get_the_ID()),
+        get_the_title(),
+        array(
+            'small' => 'f-sm-third-16x9' /* Third-width on small devices */,
+            'medium' => 'f-md-third-16x9' /* Sixth-width on medium devices */,
+            'large' => 'f-lg-third-16x9' /* Sixth-width on large devices */
+        ),
+        true
+    ); ?>
+
+    <div class="<?php echo $post_type; ?>-meta">
+        <span class="<?php echo $post_type; ?>-date"><?php the_date('j F'); ?></span>
+    </div>
+
+    <h3 class="h5 <?php echo $post_type; ?>-title"><?php the_title(); ?></h3>
+    <div class="<?php echo $post_type; ?>-excerpt"><?php the_excerpt(); ?></div>
+</a>
