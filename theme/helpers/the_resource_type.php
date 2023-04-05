cd4fc1f9ede71482c02c92d4d0ff16acfc1572d1
Mark Steadman <mark@origin.fm>
2023-04-05 17:26:50 +0100

    Allows the first letter of the resource type name to be capitalised if required

--- theme/helpers/the_resource_type.php
+++ theme/helpers/the_resource_type.php
@@ -1,4 +1,4 @@
-<?php function the_resource_type($post_id=null) {
+<?php function the_resource_type($post_id=null, $capfirst=false) {
     if($post_id === null) {
         $post_id = get_the_ID();
     }
@@ -7,11 +7,21 @@
     if (is_array($terms)) {
         foreach ($terms as $term) {
             foreach (get_term_meta($term->term_id, 'singular_name') as $name) {
-                esc_html_e($name);
+                if ($capfirst) {
+                    esc_html_e(ucfirst($name));
+                } else {
+                    esc_html_e($name);
+                }
+
                 return;
             }
 
-            esc_html_e(strtolower($term->name));
+            if ($capfirst) {
+                esc_html_e(ucfirst(strtolower($term->name)));
+            } else {
+                esc_html_e(strtolower($term->name));
+            }
+
             return;
         }
     }
