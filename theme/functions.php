970b9117069fa69f6915aeffbd6b8ede25d80500
Mark Steadman <mark@origin.fm>
2023-04-05 12:26:05 +0100

    Adds a taxonomy to give resources a “type”

--- theme/functions.php
+++ theme/functions.php
@@ -1,5 +1,5 @@
 <?php function yanaf_init_theme() {
-	$subdirs = array('classes', 'helpers', 'post_types', 'actions', 'filters');
+	$subdirs = array('classes', 'helpers', 'post_types', 'taxonomies', 'actions', 'filters');
 	$mydir = dirname(__file__);
 
 	foreach ($subdirs as $subdir) {
---
+++ theme/taxonomies/resource_type.php
@@ -0,0 +1,29 @@
+<?php register_taxonomy(
+    'resource_type',
+    'resource',
+    array(
+        'labels' => array(
+            'name' => _x('Resource Types', 'taxonomy general name'),
+            'singular_name' => _x('Resource Type', 'taxonomy singular name'),
+            'all_items' => __('All Types'),
+            'edit_item' => __('Edit Type'),
+            'view_item' => __('View Type'),
+            'update_item' => __('Update Type'),
+            'add_new_item' => __('Add New Type'),
+            'new_item_name' => __('New Type Name'),
+            'parent_item' => __('Parent Type'),
+            'parent_item_colon' => __('Parent Type:'),
+            'search_items' => __('Search Types'),
+            'popular_items' => __('Popular Types'),
+            'separate_items_with_commas' => __('Separate types with commas'),
+            'add_or_remove_items' => __('Add or remove types'),
+            'choose_from_most_used' => __('Choose from the most used types'),
+            'not_found' => __('No types found'),
+            'back_to_items' => __('Back to types')
+        ),
+        'description' => 'Types of resource',
+        'public' => true,
+        'publicly_queryable' => false,
+        'hierarchical' => true
+    )
+);
