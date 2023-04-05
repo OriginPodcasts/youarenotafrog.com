547c38450755dbc627f5cc852b8f6038a85a8eba
Mark Steadman <mark@origin.fm>
2023-04-05 11:15:30 +0100

    Finishes the host page

--- theme/page-host.php
+++ theme/page-host.php
@@ -36,7 +36,19 @@ if ($image_id = get_field('image')) {
                     <div class="page-header-container">
                         <h1 class="page-title"><?php the_title(); ?></h1>
                         <p class="page-greeting"><?php the_field('greeting'); ?></p>
-                        <?php the_field('subtitle'); ?>
+                        <?php the_field('subtitle');
+
+                        if (($links = get_field('socials')) && is_array($links) && count($links)) { ?>
+                            <ul class="host-social-links">
+                                <?php foreach ($links as $link) { ?>
+                                    <li>
+                                        <a href="<?php esc_attr_e($link['url']); ?>" title="<?php esc_attr_e(strtoupper(substr($link['site'], 0, 1)) . substr($link['site'], 1)); ?>" target="_blank">
+                                            <?php echo apply_filters('yanaf_website_icon', $link['site']); ?>
+                                        </a>
+                                    </li>
+                                <?php } ?>
+                            </ul>
+                        <?php } ?>
                     </div>
                 </div>
             </header>
@@ -45,9 +57,35 @@ if ($image_id = get_field('image')) {
 
     <div class="grid-container">
         <div class="grid-x grid-margin-x">
-            <article class="main single cell medium-9">
-                <?php the_content(); ?>
-            </article>
+            <div class="main single cell medium-9">
+                <section class="profile-section host-bio"><?php the_content(); ?></section>
+
+                <?php if ($story = get_field('story')) { ?>
+                    <section class="profile-section host-story">
+                        <h2>My story</h2>
+                        <?php echo apply_filters('the_content', $story); ?>
+                    </section>
+                <?php }
+
+                if ($experience = get_field('experience')) { ?>
+                    <section class="profile-section host-experience">
+                        <h2>Qualifications + experience</h2>
+                        <?php echo apply_filters('the_content', $experience); ?>
+                    </section>
+                <?php } 
+
+                if ($links = get_field('links')) { ?>
+                    <ul class="host-link-buttons">
+                        <?php foreach ($links as $link) { ?>
+                            <li>
+                                <a href="<?php esc_attr_e($link['url']); ?>" class="small button">
+                                    <?php esc_html_e($link['label']); ?>
+                                </a>
+                            </li>
+                        <?php } ?>
+                    </ul>
+                <?php } ?>
+            </div>
         </div>
     </div>
 </main>
