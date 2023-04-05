d176f2df554a81ac413c6814bc48d1c8b4652b66
Mark Steadman <mark@origin.fm>
2023-04-05 16:33:36 +0100

    Updates the sidebar menu to be flexible based on what’s being listed in the archive

--- theme/helpers/sidebar_menu.php
+++ theme/helpers/sidebar_menu.php
@@ -1,28 +1,54 @@
 <?php function yanaf_sidebar_menu() {
     $categories = array();
     $html = '<ul class="menu">';
+    $episode_list = (is_post_type_archive() && get_query_var('post_type') === 'episode') || get_query_var('tag') || is_tax('collection');
+    $resource_list = (is_post_type_archive() && get_query_var('post_type') === 'resource') || is_tax('resource_type');
 
-    foreach(yanaf_get_popular_post_tags() as $tag) {
-        $categories[] = array(
-            'title' => $tag->name,
-            'url' => get_term_link($tag->term_id)
+    if ($episode_list) {
+        foreach (yanaf_get_popular_post_tags() as $tag) {
+            $categories[] = array(
+                'title' => $tag->name,
+                'url' => get_term_link($tag->term_id)
+            );
+        }
+    }
+
+    $items = array();
+
+    if ($episode_list) {
+        $items[] = array(
+            'url' => get_post_type_archive_link('episode'),
+            'title' => 'Most recent'
+        );
+    } else if ($resource_list) {
+        $items[] = array(
+            'url' => get_post_type_archive_link('resource'),
+            'title' => 'Most recent'
         );
     }
 
-    $items = array(
-        array(
-            'url' => home_url('/episodes/'),
-            'title' => 'Most recent'
-        ),
-        array(
+    if (count($categories)) {
+        $items[] = array(
             'title' => 'By category',
             'children' => $categories
-        ),
-        array(
+        );
+    }
+
+    if ($resource_list) {
+        foreach (yanaf_get_resource_types() as $type) {
+            $items[] = array(
+                'title' => $type->name,
+                'url' => get_term_link($type->term_id)
+            );
+        }
+    }
+
+    if ($episode_list) {
+        $items[] = array(
             'url' => home_url('/recommends/'),
             'title' => 'Frog recommends'
-        )
-    );
+        );
+    }
 
     foreach ($items as $item) {
         $html .= yanaf_get_menu_item_html($item);
