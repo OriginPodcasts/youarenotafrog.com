8ad872979fe00d372cbdf6f1a7c6e16d5c2de18c
Mark Steadman <mark@origin.fm>
2023-04-05 11:45:34 +0100

    Prevents episodes showing up in queries where they’re not supposed to be

--- theme/actions/pre_get_posts.php
+++ theme/actions/pre_get_posts.php
@@ -1,11 +1,18 @@
 <?php add_action('pre_get_posts', 'yanaf_pre_get_posts');
-
 function yanaf_pre_get_posts($query) {
-    if (is_admin() || !$query->is_main_query()) {
+    if (is_admin() || !$query->is_main_query() || is_post_type_archive()) {
         return;
     }
 
     if (is_archive() || is_search()) {
-        $query->set('post_type', array('post', 'episode'));
+        $post_types = $query->get('post_type');
+
+        if (!$post_types || (is_array($post_types) && !count($post_types))) {
+            $post_types = array('post', 'episode');
+        } else if (is_array($post_types)) {
+            $post_types[] = 'episode';
+        }
+
+        $query->set('post_type', $post_types);
     }
 }
