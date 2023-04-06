1acf9757d7ba00f4a4b1f829b62920d5ea3b6421
Mark Steadman <mark@origin.fm>
2023-04-06 10:01:32 +0100

    Uses the new `get_resource_ctas` helper to print a list of CTAs

--- theme/includes/buttons-resource.php
+++ theme/includes/buttons-resource.php
@@ -1,4 +1,10 @@
 <ul id="links" class="resource-link-buttons">
-    <li><a href="<?php the_resource_cta_url(); ?>" class="small button" download>Signup and download</a></li>
+    <?php foreach (get_resource_ctas() as $cta) { ?>
+        <li>
+            <a href="<?php echo $cta['url']; ?>"<?php if ($cta['external']) { ?> target="_blank"<?php } ?> class="small button">
+                <?php esc_html_e($cta['label']); ?>
+            </a>
+        </li>
+    <?php } ?>
     <li><a href="<?php echo get_post_type_archive_link('resource'); ?>" class="small button">See other resources</a></li>
 </ul>
