fe62bf8d4080bf4b9e4dfffc1fe2821020ec0484
Mark Steadman <mark@origin.fm>
2023-04-06 16:32:02 +0100

    Moves contact page header to the right of the sidebar

--- theme/page-contact.php
+++ theme/page-contact.php
@@ -7,21 +7,30 @@ get_header(); ?>
         while (have_posts()) {
             the_post(); ?>
 
-            <header class="page-header">
-                <div class="grid-container">
-                    <h1 class="page-title"><?php the_title(); ?></h1>
-                </div>
-            </header>
-
             <div class="grid-container">
-                <div class="grid-x grid-margin-x">
-                    <aside class="sidebar cell medium-3">
+                <div class="grid-x grid-margin-x align-center">
+                    <aside class="sidebar cell medium-2">
                         <?php yanaf_sidebar_menu(); ?>
                     </aside>
 
-                    <article class="main single cell medium-9">
-                        <?php the_content(); ?>
-                    </article>
+                    <div class="main single cell medium-7">
+                        <header class="page-header">
+                            <div class="grid-container">
+                                <h1 class="page-title"><?php the_title(); ?></h1>
+                                <?php if ($subtitle = get_field('subtitle')) { ?>
+                                    <p class="page-subtitle"><?php esc_html_e($subtitle); ?></p>
+                                <?php }
+
+                                if ($cta = get_field('cta')) { ?>
+                                    <a href="<?php esc_attr_e($cta['url']); ?>" class="button" rel="external" target="_blank">
+                                        <?php esc_html_e($cta['label']); ?>
+                                    </a>
+                                <?php } ?>
+                            </div>
+                        </header>
+
+                        <article><?php the_content(); ?></article>
+                    </div>
                 </div>
             </div>
         <?php }
