0b1c2d7e55b0bacfeced0ba6e7812eed037f4e66
Mark Steadman <mark@origin.fm>
2023-04-06 17:10:40 +0100

    Moves the main CSS enqueue statement into the `init` hook
    It was causing horrible flashes.

--- theme/actions/wp_head.php
+++ theme/actions/wp_head.php
@@ -1,6 +1,5 @@
 <?php add_action('wp_head', 'yanaf_wp_head');
 function yanaf_wp_head() {
-    wp_enqueue_style('yanaf-style', get_stylesheet_uri());
     wp_deregister_style('acf-input');
 
     if (is_singular() && pings_open()) {
