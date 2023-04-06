8641c21075c234e77189a827095ad7845479ae1d
Mark Steadman <mark@origin.fm>
2023-04-06 15:09:40 +0100

    Adds an endpoint that lists external resources by category

--- theme/actions/init.php
+++ theme/actions/init.php
@@ -2,4 +2,5 @@
 function yanaf_init() {
     add_rewrite_rule('^download/([0-9]+)$', 'index.php?download_resource_id=$matches[1]/$', 'top');
     add_rewrite_rule('^recommends$', 'index.php?post_type=resource&external_resource=true', 'top');
+    add_rewrite_rule('^recommends-categories/([\w-]+)$', 'index.php?post_type=resource&resource_category=$matches[1]&external_resource=true', 'top');
 }
