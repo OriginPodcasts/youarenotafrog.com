a85e05308831841a5663a0d6aef23d386b2fd38e
Mark Steadman <mark@origin.fm>
2023-04-06 10:34:11 +0100

    Replaces the hardcoded “Reasons to…” text in the resource “reasons” partial with the new helper

--- theme/includes/reasons-resource.php
+++ theme/includes/reasons-resource.php
@@ -1,6 +1,6 @@
  <?php if ($reasons = get_field('reasons')) { ?>
     <section id="reasons" class="post-section resource-section resource-reasons">
-        <h2>Reasons to download this <?php the_resource_type(); ?></h2>
+        <h2><?php the_resource_reasons_header(); ?></h2>
         <div class="grid-x grid-margin-x">
             <div class="cell medium-9 medium-offset-3">
                 <?php echo apply_filters('the_content', $reasons); ?>
