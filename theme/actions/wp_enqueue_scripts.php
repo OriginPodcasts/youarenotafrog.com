0eb0096fe6a1c564939983d70224b0a4cacf6ac5
Mark Steadman <mark@origin.fm>
2023-04-06 16:39:30 +0100

    Moves the CSS registration to the `wp_head` hook

--- theme/actions/wp_enqueue_scripts.php
+++ theme/actions/wp_enqueue_scripts.php
@@ -1,6 +1,5 @@
 <?php add_action('wp_enqueue_scripts', 'yanaf_enqueue_scripts');
 function yanaf_enqueue_scripts() {
-    wp_enqueue_style('yanaf-style', get_stylesheet_uri());
     wp_dequeue_script('jquery');
     wp_deregister_script('jquery');
 
--- theme/actions/wp_head.php
+++ theme/actions/wp_head.php
@@ -1,5 +1,6 @@
 <?php add_action('wp_head', 'yanaf_wp_head');
 function yanaf_wp_head() {
+    wp_enqueue_style('yanaf-style', get_stylesheet_uri());
     if (is_singular() && pings_open()) {
         printf('<link rel="pingback" href="%s" />' . "\n", esc_url(get_bloginfo('pingback_url')));
     }
