a9ee42750aba42783819f6ab0912cfb91c963fc7
Mark Steadman <mark@origin.fm>
2023-04-06 16:39:44 +0100

    Deregisters styles for Advanced Forms

--- theme/actions/wp_head.php
+++ theme/actions/wp_head.php
@@ -1,6 +1,8 @@
 <?php add_action('wp_head', 'yanaf_wp_head');
 function yanaf_wp_head() {
     wp_enqueue_style('yanaf-style', get_stylesheet_uri());
+    wp_deregister_style('acf-input');
+
     if (is_singular() && pings_open()) {
         printf('<link rel="pingback" href="%s" />' . "\n", esc_url(get_bloginfo('pingback_url')));
     }
