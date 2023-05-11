4611d9793e7897c2c19ede5bda3e9663f5fbed0d
Mark Steadman <mark@origin.fm>
2023-05-11 12:58:22 +0100

    Picks the correct image size for episode/post lists on mobile

--- theme/includes/entry.php
+++ theme/includes/entry.php
@@ -4,7 +4,7 @@
         get_post_thumbnail_id(get_the_ID()),
         get_the_title(),
         array(
-            'small' => 'f-sm-third-16x9' /* Third-width on small devices */,
+            'small' => 'f-sm-whole-16x9' /* Full-width on small devices */,
             'medium' => 'f-md-third-16x9' /* Sixth-width on medium devices */,
             'large' => 'f-lg-third-16x9' /* Sixth-width on large devices */
         ),
