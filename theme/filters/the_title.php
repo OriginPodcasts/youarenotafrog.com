57988766ed984f00059b1b2c7d8cc05b88922127
Mark Steadman <mark@origin.fm>
2023-05-16 17:29:54 +0100

    Removes the “Private” prefix from private page titles

--- theme/filters/the_title.php
+++ theme/filters/the_title.php
@@ -3,6 +3,10 @@ function yanaf_the_title($title) {
     if ($title == '') {
         return esc_html('...');
     }
-    
+
+    if (is_singular('page') && get_post_status() === 'private') {
+        $title = str_replace('Private:', '', $title);
+    }
+
     return wp_kses_post($title);
 }
