35aabff695396d3c26ad397e5c96b989fdebcb00
Mark Steadman <mark@origin.fm>
2023-06-08 15:23:50 +0100

    Removes CSS/JS for the tag carousel

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,7 +1,6 @@
 // @codekit-prepend '../node_modules/owl-carousel/owl-carousel/owl.carousel.js';
 // @codekit-append '_reviews.js';
 // @codekit-append '_collections.js';
-// @codekit-append '_tags.js';
 // @codekit-append '_sidebar.js';
 // @codekit-append '_transcript.js';
 // @codekit-append '_newsletter-form.js';
