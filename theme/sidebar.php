37470996ec650422ad1166841df66904ae9d66bd
Mark Steadman <mark@origin.fm>
2023-06-15 17:09:20 +0100

    Adds subscription buttons to the sidebar

--- theme/sidebar.php
+++ theme/sidebar.php
@@ -1,3 +1,4 @@
 <aside class="sidebar cell medium-3">
-    <?php yanaf_sidebar_menu('<h2>Filter</h2>'); ?>
+    <?php yanaf_sidebar_menu('<h2>Filter</h2>');
+    yanaf_subscription_buttons(true, 'small'); ?>
 </aside>
