0761e08d26159721f133204585ecfb54b5561729
Mark Steadman <mark@origin.fm>
2023-06-22 15:23:03 +0100

    Pours some sugar on the search modal

--- theme/includes/search-modal.php
+++ theme/includes/search-modal.php
@@ -1,15 +1,10 @@
-<div id="search-modal" class="radius reveal" data-reveal>
-    <h2 class="h5">Search the website</h2>
-    <form action="<?php echo home_url(); ?>" class="grid-x grid-margin-x align-middle">
-        <div class="cell medium-auto">
-            <input name="s" type="text" placeholder="Search by topic or guest">
+<div id="search-modal" class="small reveal" data-reveal data-close-on-click="true" data-animation-in="fade-in" data-animation-out="fade-out">
+    <form action="<?php echo home_url(); ?>" class="grid-x">
+        <div class="cell auto">
+            <input name="s" type="search" placeholder="Search by topic or guest">
         </div>
         <div class="cell shrink">
             <button class="button">Search</button>
         </div>
     </form>
-
-    <button class="close-button" data-close aria-label="Close" type="button">
-        <span aria-hidden="true">&times;</span>
-    </button>
 </div>
