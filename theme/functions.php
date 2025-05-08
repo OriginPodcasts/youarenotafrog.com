deb5dbb61eab7efcffe3611401b5e70fe7dc47ea
Mark Steadman <mark@origin.fm>
2025-05-08 12:12:33 +0100

    Fixes WordPress lifecycle issues

--- theme/functions.php
+++ theme/functions.php
@@ -1,5 +1,5 @@
 <?php function yanaf_init_theme() {
-	$subdirs = array('classes', 'helpers', 'post_types', 'taxonomies', 'actions', 'filters', 'shortcodes', 'widgets');
+	$subdirs = array('classes', 'helpers', 'actions', 'filters', 'widgets');
 	$mydir = dirname(__file__);
 
 	foreach ($subdirs as $subdir) {
